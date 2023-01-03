7538cfcc19e05fc95f2c09dd8cb8d5e5fdd80b19
Alexander Newman <alec@rovaughn.com>
2023-01-03 10:55:12 -0700

    nix

--- flake.nix
+++ flake.nix
@@ -9,6 +9,16 @@
           buildInputs = [
             pkgs.nixfmt
             pkgs.gnumake
+            pkgs.python3
+            pkgs.ocaml
+            pkgs.dune_3
+            pkgs.ocamlPackages.ppx_deriving
+            pkgs.ocamlPackages.ounit2
+            pkgs.ocamlPackages.sexplib
+            pkgs.ocamlPackages.ppx_sexp_conv
+            pkgs.ocamlPackages.ppx_deriving
+            pkgs.ocamlPackages.batteries
+            pkgs.ocamlPackages.findlib
           ];
         };
       });
